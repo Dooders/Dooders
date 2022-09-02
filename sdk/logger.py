e44c9c5e8ab9cdce8256049870e99cf23c86c15f
Chris Mangum <peril10@hotmail.com>
2022-09-02 15:50:16 -0700

    Update logger.py

--- sdk/logger.py
+++ sdk/logger.py
@@ -2,21 +2,6 @@ import functools
 import logging
 import os
 
-
-class CustomFormatter(logging.Formatter):
-    """ Custom Formatter does these 2 things:
-    1. Overrides 'funcName' with the value of 'func_name_override', if it exists.
-    2. Overrides 'filename' with the value of 'file_name_override', if it exists.
-    """
-
-    def format(self, record):
-        if hasattr(record, 'func_name_override'):
-            record.funcName = record.func_name_override
-        if hasattr(record, 'file_name_override'):
-            record.filename = record.file_name_override
-        return super(CustomFormatter, self).format(record)
-
-
 def get_logger():
     """ Creates a Log File and returns Logger object """
 
@@ -36,8 +21,7 @@ def get_logger():
     logger = logging.Logger(log_file_name)
     logger.setLevel(logging.DEBUG)
     handler = logging.FileHandler(logPath, 'a+')
-    """ Set the formatter of 'CustomFormatter' type as we need to log base function name and base file name """
-    handler.setFormatter(CustomFormatter(
+    handler.setFormatter(logging.formatter(
         "{'timestamp': '%(asctime)s', %(message)s},"))
     logger.addHandler(handler)
 
@@ -45,22 +29,22 @@ def get_logger():
     return logger
 
 
-def log(self, granularity, scope):
-    def decorator(function):
-        @functools.wraps(function)
-        def wrapper(*args, **kwargs):
+#def log(self, granularity, scope):
+#    def decorator(function):
+ #       @functools.wraps(function)
+ #       def wrapper(*args, **kwargs):
+#
+ #           message = function(*args, **kwargs)
 
-            message = function(*args, **kwargs)
-
-            if granularity <= self.params.granularity:
-                cycle_number = self.simulation.time.time
-                unique_id = self.unique_id
-                experiment_id = self.experiment_id
-                logger_obj = get_logger()
-                prefix = f'{experiment_id} - {function.__name__} - {scope} - {cycle_number} - '
-                final_message = prefix + message
-                logger_obj.info(final_message)
-
-        return wrapper
-
-    return decorator
+ #           if granularity <= self.params.granularity:
+ #               cycle_number = self.simulation.time.time
+  #              unique_id = self.unique_id
+   #             experiment_id = self.experiment_id
+#                logger_obj = get_logger()
+#                prefix = f'{experiment_id} - {function.__name__} - {scope} - {cycle_number} - '
+#                final_message = prefix + message
+ #               logger_obj.info(final_message)
+#
+ #       return wrapper
+#
+ #   return decorator
