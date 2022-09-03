e880752fc62f5063019439159c52d041653848fe
Chris Mangum <csmangum@gmail.com>
2022-09-02 17:24:10 -0700

    refactoring

--- experiment.py
+++ experiment.py
@@ -10,12 +10,23 @@ from sdk.util import ShortUUID
 
 
 class Experiment:
+    """
+    Class to manage the overall experiment and each simulation.
+    
+    """
+    
     def __init__(self, parameters: ExperimentParameters):
         """
         Initializes an experiment.
 
         Args:
             parameters: Experiment parameters.
+            
+        Attributes:
+            experiment_id: The id of the experiment.
+            simulation: The simulation object.
+            parameters: The experiment parameters.
+            seed: The seed of the experiment. Used in experiment_id generation.
         """
         self.seed = ShortUUID()
         self.parameters = parameters
@@ -34,7 +45,7 @@ class Experiment:
         """
         self.simulation.cycle()
 
-    def get_log(self, n: int = 20):
+    def get_log(self, n: int = 20) -> List[str]:
         """ 
         Fetch the past n log entries.
         """
@@ -44,21 +55,29 @@ class Experiment:
                 if self.experiment_id in line:
                     print(line)
 
-    def get_object(self, object_id: str) -> BaseObject:  # ! make this better
+    def get_object(self, object_id: str) -> BaseObject:
         """ 
         Fetch an object by its id.
 
         Args:
-            object_id: The id of the object. Based on a random short uuid assigned to every object at its creation.
+            object_id: The id of the object. 
+                Based on a random short uuid assigned to every object at its creation.
+        
+        Returns:
+            The object with the given id.
         """
         return self.simulation.environment.get_object(object_id)
     
     def get_objects(self, object_type: str ='BaseObject') -> List[BaseObject]:
         """ 
         Get all objects of a given type.
+        Returns all objects if no type is given.
 
         Args:
             type: The type of object to get.
+            
+        Returns:
+            A list of objects of the given type.
         """
         return self.simulation.environment.get_objects(object_type)
     
@@ -66,10 +85,14 @@ class Experiment:
     def get_random_objects(self, object_type: str ='BaseObject', n: int = 1) -> List[BaseObject]:
         """ 
         Get n random objects of a given type.
+        Returns all objects if no type is given.
 
         Args:
             type: The type of object to get.
             n: The number of objects to get.
+            
+        Returns:
+            A list of n random objects of the given type.
         """
         object_list = self.get_objects(object_type)
         k = min(len(object_list), n)
@@ -89,9 +112,11 @@ class Experiment:
     def get_dooder_history(self, object_id: str = 'Random') -> Dict:
         """
         Get the history of a dooder.
+        Returns a random dooder history if no id is given.
         
         Args:
-            object_id: The id of the dooder. If 'Random', a random dooder will be selected. 
+            object_id: The id of the dooder. If 'Random', 
+                a random dooder will be selected. 
         
         Returns:
             A dictionary of the history of the dooder.
@@ -124,26 +149,72 @@ class Experiment:
 
 
 class SessionManager:
+    """ 
+    Class to manage distinct sessions and interact with the experiments.
+    """
     def __init__(self):
+        """ 
+        Initializes the session manager.
+        
+        Attributes:
+            active_experiments: A dictionary of active experiments.
+            active_connections: A list of active websocket objects.
+        """
         self.active_connections: List[WebSocket] = []
         self.active_experiments: Dict[str, Experiment] = {}
 
-    def add_experiment(self, experiment: Experiment):
+    def add_experiment(self, experiment: Experiment) -> None:
+        """ 
+        Add an experiment to the active experiments.
+        """
         self.active_experiments[experiment.experiment_id] = experiment
 
-    def get_experiment(self, experiment_id: str):
+    def get_experiment(self, experiment_id: str) -> Experiment:
+        """ 
+        Get an experiment by its id.
+        
+        Args:
+            experiment_id: The id of the experiment.
+        
+        Returns:
+            The experiment with the given id.
+        """
         return self.active_experiments[experiment_id]
 
-    def delete_experiment(self, experiment_id: str):
+    def delete_experiment(self, experiment_id: str) -> None:
+        """
+        Delete an experiment from the active experiments.
+        
+        Args:
+            experiment_id: The id of the experiment.
+        """
         del self.active_experiments[experiment_id]
 
-    async def connect(self, websocket: WebSocket):
+    async def connect(self, websocket: WebSocket) -> None:
+        """ 
+        Connect a websocket to the session manager.
+        
+        Args:
+            websocket: The websocket to connect.
+        """
         await websocket.accept()
         self.active_connections.append(websocket)
 
-    def disconnect(self, websocket: WebSocket):
+    def disconnect(self, websocket: WebSocket) -> None:
+        """ 
+        Disconnect a websocket from the session manager.
+        
+        Args:
+            websocket: The websocket to disconnect.
+        """
         self.active_connections.remove(websocket)
 
-    def cleanup(self, session_id: str):
+    def cleanup(self, session_id: str) -> None:
+        """  
+        Cleanup the session manager.
+        
+        Args:
+            session_id: The id of the session to cleanup.
+        """
         self.disconnect(self.active_connections[session_id])
         self.delete_experiment(session_id)
