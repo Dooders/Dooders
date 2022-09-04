ff42e4261b9548f808fdb2163853594df4e36203
Chris Mangum <csmangum@gmail.com>
2022-09-04 13:41:02 -0700

    added doc

--- sdk/information/collectors.py
+++ sdk/information/collectors.py
@@ -1,12 +1,32 @@
+""" 
+The CollectorRegistry is a global registry of collectors. A new collector
+is added through the register_collector() decorator. 
+
+The decorator takes the name of the collector and component as arguments. The
+component indicates what component or module the collector belongs to. The 
+collector name is used to identify the collector in the output.
+
+A collector serves as a method to extract information from the simulation 
+at the end of each cycle. The information is stored in a dictionary, which 
+is then passed to the Information component.
+
+A collector can return any type of information. Every Collector must input 
+'simulation' as an argument. The simulation object is used to extract 
+information from the simulation.
+"""
+
 from statistics import mean
 from typing import Callable
 
 
 class CollectorRegistry:
     """ 
-    The factory class for creating executors
+    The factory class for creating collectors
+
+    The CollectorRegistry is a global registry of collectors. A new collector
+    is added through the register_collector() decorator.
     """
-    
+
     # The registry of collectors
     registry = []
 
@@ -14,11 +34,11 @@ class CollectorRegistry:
     def register_collector(cls, name: str, component: str) -> Callable:
         """ 
         Register a collector in the registry.
-        
+
         Args:
             name: Name of the collector.
             component: Component of the collector.
-            
+
         Returns:
             The decorator function.
         """
@@ -30,15 +50,22 @@ class CollectorRegistry:
 
         return inner_wrapper
 
+
+# ===========================
+# Base Collectors
+# ===========================
+
 @CollectorRegistry.register_collector('DooderCount', 'Simulation')
 def get_dooder_count(simulation) -> int:
     """Return the number of dooders in the simulation."""
     return simulation.time.get_object_count('Dooder')
 
+
 @CollectorRegistry.register_collector('EnergyCount', 'Simulation')
 def get_energy_count(simulation) -> int:
     """Return the number of energy in the simulation."""
     return simulation.time.get_object_count('Energy')
+
 
 @CollectorRegistry.register_collector('DirectionCounts', 'Simulation')
 def get_direction_counts(simulation) -> dict:
@@ -51,6 +78,7 @@ def get_direction_counts(simulation) -> dict:
 
     return dict(direction_counts)
 
+
 @CollectorRegistry.register_collector('TotalDooderEnergySupply', 'Simulation')
 def get_total_energy_supply(simulation) -> int:
     """ Return the total energy supply of all dooders in the simulation. """
@@ -62,6 +90,7 @@ def get_total_energy_supply(simulation) -> int:
 
     return sum(energy_supply)
 
+
 @CollectorRegistry.register_collector('AverageEnergyAge', 'Simulation')
 def get_average_energy_age(simulation) -> float:
     """Return the average age of energy in the simulation."""
@@ -72,6 +101,7 @@ def get_average_energy_age(simulation) -> float:
         return 0
 
     return round(mean(energy_age), 2)
+
 
 @CollectorRegistry.register_collector('AverageBehaviorProfile', 'PostCollect')
 def get_average_behavior_profile(simulation) -> dict:
