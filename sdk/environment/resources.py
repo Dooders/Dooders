b24a2a54e0fdb871e8c9bc6f3873d83cbd8844f9
Chris Mangum <peril10@hotmail.com>
2022-09-15 14:12:44 -0700

    add docstrings and type checking

--- sdk/environment/resources.py
+++ sdk/environment/resources.py
@@ -1,5 +1,17 @@
-from sdk.strategies.strategies import Strategies
+""" 
+Resources
+---------
+This module contains the resources allocated in the environment and used by the
+agents. It also contains the functions to access the resources.
+"""
+
+from typing import TYPE_CHECKING
+
 from sdk.environment.energy import Energy
+from sdk.strategies.strategies import Strategies
+
+if TYPE_CHECKING:
+    from sdk.simulation import Simulation
 
 strategy = {
     'EnergyPerCycle': {'function': 'uniform_distribution', 'args': {'low': 10, 'high': 15}},
@@ -9,21 +21,53 @@ strategy = {
 }
 
 class Resources:
+    """ 
+    Resources are the objects that are consumed by agents to perform actions.
+    They are generated and placed in random locations based on the provided strategy.
+    
+    Args:
+    simulation : Simulation object
+        The simulation object that contains the environment and the agents.
+    
+    Attributes:
+        simulation: see ``Args`` section.
+        available_resources (dict): A dictionary of *current* available resources indexed by their unique id.
+        total_allocated_energy (int): The total number of allocated resources.
+    """
     
     available_resources = {}
     total_allocated_energy = 0
     
-    def __init__(self, simulation):
+    def __init__(self, simulation: 'Simulation'):
         self.simulation = simulation
 
-    def generation_strategy(self, variable):
+    def generation_strategy(self, variable: str):
+        """ 
+        Generates a value for the given variable based on the provided strategy.
+        
+        Args:
+            variable (str): The name of the variable to generate a value for.
+        
+        Returns:
+            The generated value.
+        """
         strat = strategy[variable]['function']
         args = strategy[variable]['args']
         func = Strategies.get(strat, 'Generation')
         
         return round(func(**args))
 
-    def placement_strategy(self, simulation, number):
+    def placement_strategy(self, simulation: 'Simulation', number: int):
+        """ 
+        Generates a list of locations for the given number of resources and based on the provided strategy.
+        
+        Args:
+            simulation (Simulation): The simulation object that contains the environment and the agents.
+            number (int): The number of resources to generate locations for.
+        
+        Returns:   
+            A list of locations.
+        """
         strat = strategy['EnergyPlacement']['function']
         func = Strategies.get(strat, 'Placement')
         args = (simulation, number)
@@ -31,6 +75,12 @@ class Resources:
         return func(*args)
     
     def allocate_resources(self):
+        """ 
+        Allocates resources based on the provided strategy.
+        
+        Returns:
+            The number of allocated resources.
+        """
         energy_per_cycle = self.generation_strategy('EnergyPerCycle')
         max_total_energy = self.generation_strategy('MaxTotalEnergy')
         energy_lifespan = self.generation_strategy('EnergyLifespan')
@@ -45,15 +95,32 @@ class Resources:
                 self.total_allocated_energy += 1
                 
     def step(self):
+        """ 
+        Performs a step in the simulation.
+        """
         for resource in list(self.available_resources.values()):
             resource.step()
             
         self.allocate_resources()
             
-    def consume(self, resource):
+    def consume(self, resource: Energy):
+        """ 
+        Consumes the given resource.
+        
+        Args:
+            resource (Energy): The resource to consume.
+        """
         self.simulation.environment.remove_object(resource)
         self.available_resources.pop(resource.unique_id)
         
-    def log(self, granularity, message, scope):
+    def log(self, granularity: int, message: str, scope: str):
+        """ 
+        Logs the given message.
+        
+        Args:
+            granularity (str): The granularity of the message.
+            message (str): The message to log.
+            scope (str): The scope of the message.
+        """
         self.simulation.log(granularity, message, scope)
         
