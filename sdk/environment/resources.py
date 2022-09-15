c19296af7a55c902c0f78f1be225688b49c9d0c1
Chris Mangum <csmangum@gmail.com>
2022-09-14 21:10:31 -0700

    updates

--- sdk/environment/resources.py
+++ sdk/environment/resources.py
@@ -1,65 +1,131 @@
+from abc import ABCMeta, abstractmethod
 from random import choices
+from typing import Callable, Union
 
 from pydantic import BaseModel
 from scipy.stats import norm, randint
 
+
+class BaseStrategy(BaseModel, metaclass=ABCMeta):
+    """
+    Base class for strategies.
+    """
+
+    @abstractmethod
+    def __call__(self, *args, **kwargs) -> Union[int, float, str]:
+        raise NotImplementedError
+
+
 strategy = {
-    'EnergyPerCycle': {'function': 'Uniform', 'args': {'low': 1, 'high': 10}},
-    'MaxTotalEnergy': {'function': 'Normal', 'args': {'mean': 50, 'std': 10}},
-    'EnergyLifespan': {'function': 'Fixed', 'args': {'value': 7}},
-    'EnergyPlacement': {'function': 'Random'}
+    'EnergyPerCycle': {'function': 'UniformDistribution', 'args': {'low': 1, 'high': 10}},
+    'MaxTotalEnergy': {'function': 'NormalDistribution', 'args': {'mean': 50, 'std': 10}},
+    'EnergyLifespan': {'function': 'FixedValue', 'args': {'value': 7}},
+    'EnergyPlacement': {'function': 'RandomLocation'}
 }
 
 
-class Strategies(BaseModel):
+class Strategies:
 
-    @staticmethod
-    def uniform_distribution(low: int, high: int, size: int = 1):
-        return randint.rvs(low=low, high=high, loc=0, size=size)
+    strategies = {
+        'Generation': {},
+        'Placement': {},
+    }
 
-    @staticmethod
-    def normal_distribution(mean: int, std: int, size: int = 1):
-        return norm.rvs(loc=mean, scale=std, size=size)
+    @classmethod
+    def register(cls, type) -> Callable:
+        """ 
+        Register a collector in the registry.
+        Args:
+            name: Name of the collector.
+            component: Component of the collector.
+        Returns:
+            The decorator function.
+        """
+        def inner_wrapper(wrapped_class: Callable) -> Callable:
+            cls.strategies[type][wrapped_class.__name__] = wrapped_class
+            return wrapped_class
 
-    @staticmethod
-    def fixed_value(value):
-        return [value]
+        return inner_wrapper
 
-    @staticmethod
-    def random_selection(simulation, number):
+    @classmethod
+    def get(cls, strategy, type) -> Callable:
+        """ 
+        Get a collector from the registry.
+        Args:
+            name: Name of the collector.
+            component: Component of the collector.
+        Returns:
+            The collector.
+        """
+        return cls.strategies[type][strategy]
+
+
+@Strategies.register("Generation")
+class UniformDistribution(BaseStrategy):
+    #! test this
+    low: int = 1
+    high: int = 10
+
+    def __call__(self) -> int:
+        return randint.rvs(low=self.low, high=self.high)
+    
+
+@Strategies.register("Generation")
+class NormalDistribution(BaseStrategy):
+    #! test this
+    mean: int = 50
+    std: int = 10
+
+    def __call__(self) -> int:
+        return norm.rvs(loc=self.mean, scale=self.std)
+    
+@Strategies.register("Generation")
+class FixedValue(BaseStrategy):
+    #! test this
+    value: int = 7
+
+    def __call__(self) -> int:
+        return self.value
+    
+@Strategies.register("Placement")
+class RandomLocation(BaseStrategy):
+    #! test this
+    def __call__(self, simulation, number) -> list:
         locations = [(loc[1], loc[2])
                      for loc in simulation.environment.coord_iter()]
         random_locations = choices(locations, k=number)
 
         return random_locations
 
+# class BaseStrategy(BaseModel):
+#     " Make this a decorator with the get method "
 
-class BaseStrategy(BaseModel):
-    " Make this a decorator with the get method "
-
-    @classmethod
-    def get(cls, strategy):
-        return getattr(cls, strategy)
-
-
-class GenerationStrategies(BaseStrategy):
-    Uniform = Strategies.uniform_distribution
-    Normal = Strategies.normal_distribution
-    Fixed = Strategies.fixed_value
+#     @classmethod
+#     def get(cls, strategy):
+#         return getattr(cls, strategy)
 
 
-class PlacementStrategies(BaseStrategy):
-    Random = Strategies.random_selection
+# class GenerationStrategies(BaseStrategy):
+#     Uniform = Strategies.uniform_distribution
+#     Normal = Strategies.normal_distribution
+#     Fixed = Strategies.fixed_value
+
+
+# class PlacementStrategies(BaseStrategy):
+#     Random = Strategies.random_selection
 
 
 class Resources:
 
     def generation_strategy(self, variable):
         strat = strategy[variable]['function']
-        func = GenerationStrategies.get(strat)
+        func = Strategies.get(strat, 'Generation')
         args = strategy[variable]['args']
 
         return round(func(**args)[0])
 
-    def placement_strategy(self, number):
-        pass
+    def placement_strategy(self, simulation, number):
+        func = Strategies.get('RandomLocation', 'Placement')
+        args = (simulation, number)
+
+        return func(*args)
