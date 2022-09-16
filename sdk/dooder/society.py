219c7972d3f3341c66c4fc79bf13ee4bbb04e245
Chris Mangum <csmangum@gmail.com>
2022-09-15 20:40:42 -0700

    expanding society

--- sdk/dooder/society.py
+++ sdk/dooder/society.py
@@ -1,64 +1,158 @@
 import networkx as nx
-
 from sdk.dooder import Dooder
+from sdk.strategies.strategies import Strategies
 
 """ 
-SeedStrategy
-    SeedCount
-    SeedPlacement
-    SeedGenetics
-DooderStrategy
-    StartingEnergySupply
-    MaxEnergySupply
-    Metabolism
-    SurvivalProbability
-    ReproductionProbability
-    ReproductionEnergyCost
-    ReproductionSuccessProbability
-    MoveProbability
-    MoveSuccessProbability
-    MoveEnergyCost
-    
-    
-Per dooder:
-- position
-- id
-
 Graph:
     nodes: dooders (id, age, position, object_ref)
     edges: interactions (cycle number, involved dooders, interaction type) & overall count per dooder
-
 """
+
+seed_strategy = {
+    'SeedCount': {
+        'function': 'uniform',
+        'args': {
+            'low': 10,
+            'high': 15
+        }
+    },
+    'SeedPlacement': {
+        'function': 'random_location'
+    },
+    'SeedGenetics': {
+        'function': 'random_genetics'
+    }
+}
+
+
+dooder_strategy = {
+    'StartingEnergySupply': {
+        'function': 'uniform',
+        'args': {
+            'low': 10,
+            'high': 15
+        }
+    },
+    'MaxEnergySupply': {
+        'function': 'normal',
+        'args': {
+            'mean': 50,
+            'std': 10
+        }
+    },
+    'Metabolism': {
+        'function': 'uniform',
+        'args': {
+            'low': 1,
+            'high': 5
+        }
+    },
+    'SurvivalProbability': {
+        'function': 'fixed_value',
+        'args': {
+            'value': 0.5
+        }
+    },
+    'ReproductionProbability': {
+        'function': 'fixed_value',
+        'args': {
+            'value': 0.5
+        }
+    },
+    'ReproductionEnergyCost': {
+        'function': 'fixed_value',
+        'args': {
+            'value': 10
+        }
+    },
+    'ReproductionSuccessProbability': {
+        'function': 'fixed_value',
+        'args': {
+            'value': 0.5
+        }
+    },
+    'MoveProbability': {
+        'function': 'fixed_value',
+        'args': {
+            'value': 0.5
+        }
+    },
+    'MoveSuccessProbability': {
+        'function': 'fixed_value',
+        'args': {
+            'value': 0.5
+        }
+    },
+    'MoveEnergyCost': {
+        'function': 'fixed_value',
+        'args': {
+            'value': 1
+        }
+    }
+}
 
 
 class Society:
-    
+
     active_dooders = {}
-    dooder_graph = nx.Graph()
+    graph = nx.Graph()
     total_created_dooders = 0
-    
+
     def __init__(self, simulation):
         self.simulation = simulation
-    
-    def generate_seed_population(self, seed_strategy):
-        pass
-    
-    def generate_dooder(self, dooder_strategy):
-        pass 
-    
-    def place_dooder(self, dooder_id, position):
-        pass
-    
+
+    def generation_strategy(self, variable):
+        #! maybe make this a more general function inside the Strategies class
+        #! call it compile_strategy or something
+        #! maybe compile all at once?????
+        strat = seed_strategy[variable]['function']
+        args = seed_strategy[variable]['args']
+        func = Strategies.get(strat, 'Generation')
+
+        return round(func(**args))
+
+    def placement_strategy(self, simulation, number):
+        strat = seed_strategy['SeedPlacement']['function']
+        func = Strategies.get(strat, 'Placement')
+        args = (simulation, number)
+
+        return func(*args)
+
+    def generate_seed_population(self):
+        seed_count = self.generation_strategy('SeedCount')
+        seed_placement = self.placement_strategy(self.simulation, seed_count)
+
+        for position in seed_placement:
+            self.generate_dooder(position, None)
+
+    def generate_dooder(self, position, dooder_strategy):
+        #! need to fix the dooder object creation and simplify it
+        #! maybe make a dooder factory class or input dict
+        dooder = Dooder(self.simulation.generate_id(), position,
+                        self.simulation, dooder_strategy)
+        self.place_dooder(dooder, position)
+
+    def place_dooder(self, dooder, position):
+        self.simulation.environment.place_object(dooder, position)
+        self.simulation.time.add(dooder)
+
+        self.active_dooders[dooder.unique_id] = dooder
+        # ! add dooder attributes to node
+        self.graph.add_node(dooder.unique_id)
+        self.total_created_dooders += 1
+
     def terminate_dooder(self, dooder_id):
-        pass
-    
+        self.active_dooders.pop(dooder_id)
+        self.simulation.time.remove(dooder_id)
+        self.simulation.environment.remove_object(dooder_id)
+
     def get_dooder(self, dooder_id):
-        pass
-    
+        return self.active_dooders[dooder_id]
+
     @property
     def active_dooder_count(self):
         return len(self.active_dooders)
-    
+
     @property
     def total_dooder_count(self):
         return self.total_created_dooders
