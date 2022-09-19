4704d119159279e11fb3438b405c10c92d6106cc
Chris Mangum <peril10@hotmail.com>
2022-09-19 16:29:00 -0700

    added tests

--- tests/test_resources.py
+++ tests/test_resources.py
@@ -1,1 +1,46 @@
+import pytest
+from sdk.environment.resources import Resources
 
+from tests.test_util import mock_simulation
+
+
+@pytest.fixture
+def resources():
+    simulation = mock_simulation()
+    resources = Resources(simulation)
+
+    return resources, simulation
+
+
+def test_init():
+    simulation = mock_simulation()
+    resources = Resources(simulation)
+    assert resources.available_resources == {}
+    assert resources.total_allocated_energy == 0
+
+
+def test_allocate_resources(resources):
+    resources, _ = resources
+    resources.allocate_resources()
+    assert resources.available_resources != {}
+    assert resources.total_allocated_energy > 0
+
+
+def test_step(resources):
+    resources, _ = resources
+    resources.allocate_resources()
+    assert resources.available_resources != {}
+    assert resources.total_allocated_energy > 0
+    available_before = len(resources.available_resources)
+    total_before = resources.total_allocated_energy
+    resources.step()
+    assert len(resources.available_resources) > available_before
+    assert resources.total_allocated_energy > total_before
+
+
+def test_remove(resources):
+    resources, _ = resources
+    resources.allocate_resources()
+    energy = list(resources.available_resources.values())[0]
+    resources.remove(energy)
+    assert energy.unique_id not in resources.available_resources
---
+++ tests/test_strategies.py
@@ -0,0 +1,18 @@
+from sdk.strategies.strategies import Strategies
+
+
+def test_register():
+    @Strategies.register('Generation')
+    class TestStrategy:
+        pass
+
+    assert 'TestStrategy' in Strategies.strategies['Generation']
+    assert Strategies.strategies['Generation']['TestStrategy'] == TestStrategy
+    
+    
+def test_get():
+    @Strategies.register('Generation')
+    class TestStrategy:
+        pass
+
+    assert Strategies.get('TestStrategy', 'Generation') == TestStrategy
