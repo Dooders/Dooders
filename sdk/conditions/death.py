93fa4412c58170248d4c604002a1fdf12a20974d
Chris Mangum <csmangum@gmail.com>
2022-09-21 00:04:10 -0700

    adding death condition

--- sdk/conditions/death.py
+++ sdk/conditions/death.py
@@ -1,1 +1,21 @@
+from sdk.conditions.conditions import Conditions
 
+
+@Conditions.register('starvation')
+def starvation(dooder) -> bool: 
+    #! maybe have a process that you supply attribute and default value, and if it doesnt exsist in the dooder object, it creates it   
+    # create a days hungry attribute if it doesn't exist
+    if not hasattr(dooder, 'days_hungry'):
+        dooder.days_hungry = 0
+        
+    # if the dooder is hungry, increment the days hungry attribute  
+    if dooder.energy_supply == 0:
+        dooder.days_hungry += 1
+    else:
+        dooder.days_hungry = 0
+    
+    # if the dooder has been hungry for 3 days, return true
+    if dooder.days_hungry >= 3:
+        return True
+    else:
+        return False
