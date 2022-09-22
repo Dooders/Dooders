325a47b48af02c1d5800971275e43bc6e340f62b
Chris Mangum <peril10@hotmail.com>
2022-09-22 13:53:14 -0700

    clean fate

--- sdk/dooder/fate.py
+++ sdk/dooder/fate.py
@@ -1,59 +1,13 @@
-from random import randint, randrange, sample
-from typing import List
+from random import randint
+
 
 class Fate:
     """ 
     Fate class used to decide the "fate" of an action.
+    #! Do I need this class when I have strategies and conditions framework???
+    #! or maybe have a fate strategy
     """
-
-    @classmethod
-    def generate_probability(cls) -> int:
-        """  
-        Generate a random probability
-
-        Returns:
-            int: A random probability between 0 and 100
-        """
-        return randint(1, 100)
         
-
-    @classmethod
-    def generate_weights(cls) -> List[int]:
-        """ 
-        Generate a list of random weights
-
-        TBD
-        """
-
-        return [None]
-
-    @classmethod
-    def generate_score(cls) -> int:
-        """ 
-        Generate a random score
-
-        Returns:
-            int: A random score between 0 and 100
-        """
-
-        return randrange(1, 100)
-
-    @classmethod
-    def generate_distribution(cls, number_of_choices: int) -> List[int]:
-        """
-        Generates a distribution of probabilities for a given number of choices.
-
-        Args:
-            number_of_choices: The number of choices to generate probabilities for.
-
-        Returns:
-            List[int]: A list of probabilities for the given number of choices. Adding up to 100.
-        """
-
-        #! TODO: apply different distributions
-        dividers = sorted(sample(range(1, 100), number_of_choices - 1))
-        return [a - b for a, b in zip(dividers + [100], [0] + dividers)]
-
     @classmethod
     def ask_fate(cls, probability: int) -> bool:
         """ 
@@ -64,6 +18,8 @@ class Fate:
 
         Returns:
             bool: True if the action is successful, False otherwise.
+            
+        #TODO: Determine different distributions to check against the supplied probability.
         """
 
         if randint(1, 100) < probability:
--- tests/test_fate.py
+++ tests/test_fate.py
@@ -1,14 +1,5 @@
 from sdk.dooder.fate import Fate
-
-
-def test_fate():
-    assert type(Fate.generate_probability()) == int
-    assert type(Fate.generate_score()) == int
-    assert type(Fate.generate_distribution(2)) == list
-    assert len(Fate.generate_distribution(2)) == 2
-    assert sum(Fate.generate_distribution(2)) == 100
-    assert Fate.generate_score() in range(0, 100)
-    assert Fate.generate_probability() in range(0, 100)
+    
     
 def test_ask_fate():
     assert type(Fate.ask_fate(50)) == bool
