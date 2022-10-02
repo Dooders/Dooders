82746929dcf086860e972b01d6c91effa6c433dc
Chris Mangum <csmangum@gmail.com>
2022-10-02 13:44:21 -0700

    Update dooder.py

--- sdk/collectors/dooder.py
+++ sdk/collectors/dooder.py
@@ -1,1 +1,11 @@
+from sdk.core.collector import Collector
 
+@Collector.register('DooderStats')
+def get_stats(simulation):
+  
+  dooder_stats = []
+  
+  for dooder in simulation.society.active_dooders.values():
+    dooder_stats.append(dooder.stats)
+    
+  return dooder_stats
