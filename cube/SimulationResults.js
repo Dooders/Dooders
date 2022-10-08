35f23fddd23c454a863674dd4e59de28833a32ee
Chris Mangum <peril10@hotmail.com>
2022-10-08 15:32:04 -0700

    update schema

--- cube/SimulationResults.js
+++ cube/SimulationResults.js
@@ -1,43 +1,56 @@
 cube(`SimulationResults`, {
-    sql: `SELECT * FROM public."SimulationResults"`,
-    
-    preAggregations: {
-      // Pre-Aggregations definitions go here
-      // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started  
+  sql: `SELECT * FROM public."SimulationResults"`,
+
+  preAggregations: {
+    // Pre-Aggregations definitions go here
+    // Learn more here: https://cube.dev/docs/caching/pre-aggregations/getting-started
+  },
+
+  joins: {},
+
+  measures: {
+    count: {
+      type: `count`,
+      drillMembers: [experimentid, cyclenumber],
     },
-    
-    joins: {
-      
+
+    doodercount: {
+      sql: `${CUBE}."DooderCount"`,
+      type: `sum`,
     },
-    
-    measures: {
-      count: {
-        type: `count`,
-        drillMembers: [experimentid]
-      },
-      
-      cyclenumber: {
-        sql: `${CUBE}."CycleNumber"`,
-        type: `sum`
-      },
-      
-      doodercount: {
-        sql: `${CUBE}."DooderCount"`,
-        type: `sum`
-      },
-      
-      energycount: {
-        sql: `${CUBE}."EnergyCount"`,
-        type: `sum`
-      }
+
+    energycount: {
+      sql: `${CUBE}."EnergyCount"`,
+      type: `sum`,
     },
-    
-    dimensions: {
-      experimentid: {
-        sql: `${CUBE}."ExperimentID"`,
-        type: `string`
-      }
+
+    totalenergysupply: {
+      sql: `${CUBE}."TotalDooderEnergySupply"`,
+      type: `sum`,
     },
-    
-    dataSource: `default`
-  });
+
+    avgenergysupply: {
+      sql: `${CUBE}."TotalDooderEnergySupply"`,
+      type: `avg`,
+    },
+
+    energyage: {
+      sql: `${CUBE}."AverageEnergyAge"`,
+      type: `avg`,
+    },
+  },
+
+  dimensions: {
+    experimentid: {
+      sql: `${CUBE}."ExperimentID"`,
+      type: `string`,
+    },
+
+    cyclenumber: {
+      sql: `${CUBE}."CycleNumber"`,
+      type: `number`,
+    },
+  },
+
+  dataSource: `default`,
+});
