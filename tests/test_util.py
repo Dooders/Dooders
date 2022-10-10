ca3a7d71241aedba2f721ae88782b9d4fe8305b3
Chris Mangum <csmangum@gmail.com>
2022-10-09 21:42:46 -0700

    Update test_util.py

--- tests/test_util.py
+++ tests/test_util.py
@@ -4,6 +4,7 @@ from sdk.models.energy import Energy
 from sdk.simulation import Simulation
 from sdk.models.resources import Resources
 
+#! do i need a test config?
 
 # with open("sdk/config.json", "r") as f:
 #      test_config = json.load(f)
