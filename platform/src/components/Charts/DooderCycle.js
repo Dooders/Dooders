1ee320698ffe3bb4343cfed683ce06976d096aec
Chris Mangum <peril10@hotmail.com>
2022-10-18 18:18:06 -0700

    updates

---
+++ platform/src/components/Charts/DooderCycle.js
@@ -0,0 +1,51 @@
+import { useCubeQuery } from "@cubejs-client/react";
+import {
+  XAxis,
+  CartesianGrid,
+  YAxis,
+  Tooltip,
+  Legend,
+  LineChart,
+  Line,
+  ResponsiveContainer,
+} from "recharts";
+
+const DooderCycle = () => {
+  const { resultSet, isLoading, error, progress } = useCubeQuery({
+    measures: ["SimulationResults.DooderCount"],
+    dimensions: ["SimulationResults.CycleNumber"],
+    order: [["SimulationResults.CycleNumber", "asc"]],
+  });
+  if (isLoading) {
+    return (
+      <div>
+        {(progress && progress.stage && progress.stage.stage) || "Loading..."}
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div>{error.toString()}</div>;
+  }
+
+  if (!resultSet) {
+    return null;
+  }
+
+  return (
+    <div>
+      <ResponsiveContainer width="40%" height={350}>
+        <LineChart data={resultSet.series()[0].series}>
+          <CartesianGrid strokeDasharray="5 5" />
+          <XAxis dataKey="x"></XAxis>
+          <YAxis />
+          <Tooltip />
+          <Legend verticalAlign="top" height={36} />
+          <Line name="DooderCount" dataKey="value" fill="#8884d8" />
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  );
+};
+
+export default DooderCycle;
--- platform/src/components/ExperimentMain.js
+++ platform/src/components/ExperimentMain.js
@@ -1,157 +1,62 @@
 import Toolbar from "./Toolbar/Toolbar";
 import Dashboard from "./Dashboard/Dashboard";
-import Box from '@mui/material/Box';
+import Box from "@mui/material/Box";
 import ExperimentChart from "./ExperimentChart/ExperimentChart";
-import {
-  useState,
-  useEffect,
-  useRef
-} from "react";
-import { useCubeQuery } from '@cubejs-client/react';
-import { BarChart, XAxis, CartesianGrid, YAxis, Tooltip, Bar, Legend } from "recharts";
-
-
-const ChartApp = () => {
-
-  const { resultSet, isLoading, error, progress } = useCubeQuery({
-    measures: ["SimulationResults.DooderCount"],
-    dimensions: ["SimulationResults.CycleNumber"],
-    order: [["SimulationResults.CycleNumber","asc"]]
-  });
-  if (isLoading) {
-    return (
-      <div>
-        {(progress && progress.stage && progress.stage.stage) || "Loading..."}
-      </div>
-    );
-  }
-
-  if (error) {
-    return <div>{error.toString()}</div>;
-  }
-
-  if (!resultSet) {
-    return null;
-  }
-
-  return (
-    <div>
-      <BarChart width={1200} height={350} data={resultSet.series()[0].series}>
-      <CartesianGrid strokeDasharray="5 5" />
-      <XAxis dataKey="x">
-      </XAxis>
-      <YAxis />
-      <Tooltip />
-      <Legend verticalAlign="top" height={36} />
-      <Bar name="DooderCount" dataKey="value" fill="#8884d8" />
-      </BarChart>
-    
-      </div>
-);
-};
-
-        
-
-
-// export default ChartApp;
-
+import { useState, useEffect, useRef } from "react";
+import DooderCycle from "./Charts/DooderCycle";
+import EnergyCycle from "./Charts/EnergyCycle";
+import Grid from '@mui/material/Grid';
 
 const App = () => {
-    const [sessionData, setSessionData] = useState([]);
-    const [previousResults, setPreviousResults] = useState([]);
-    const webSocket = useRef(null);
+  const [sessionData, setSessionData] = useState([]);
+  const [previousResults, setPreviousResults] = useState([]);
+  const webSocket = useRef(null);
 
-    // const { resultSet, isLoading, error, progress } = useCubeQuery({
-    // measures: ["SimulationResults.DooderCount"],
-    // dimensions: ["SimulationResults.CycleNumber"],
-    // order: [["SimulationResults.CycleNumber","asc"]]});
+  useEffect(() => {
+    webSocket.current = new WebSocket("ws://localhost:8080/ExperimentData");
+    webSocket.current.onopen = () => {
+      console.log("Connected to server");
+    };
+    webSocket.current.onmessage = (event) => {
+      const data = JSON.parse(event.data);
+      // console.log(data);
+      setSessionData((current) => [...current, data]);
+    };
+    webSocket.current.onclose = () => {
+      console.log("Disconnected from server");
+      webSocket.current.close();
+    };
+    return () => {
+      webSocket.current.close();
+    };
+  }, []);
 
-    // console.log(resultSet);
+  // console.log(sessionData[sessionData.length - 1]);
 
-    // if (isLoading) {
-    //   return (
-    //     <div>
-    //       {(progress && progress.stage && progress.stage.stage) || "Loading..."}
-    //     </div>
-    //   );
-    // }
-  
-    // if (error) {
-    //   return <div>{error.toString()}</div>;
-    // }
-  
-    // if (!resultSet) {
-    //   return null;
-    // }
-  
-    // //Transform data for visualization
-    // const labels = resultSet
-    //   .seriesNames({
-    //     x: [],
-    //     y: ["Orders.createdAt"]
-    //   })
-    //   .map((column) => (column.value ? column.value : column.key));
-  
-    // const datasets = resultSet.series().map((item, i) => {
-    //   return {
-    //     label: item.title,
-    //     data: item.series.map((item) => item.value)
-    //   };
-    // });
-
-
-    // const sessionData = [
-    //   { StepCount: 0, AgentCount: 4000 },
-    //   { StepCount: 1, AgentCount: 1243 },
-    //   { StepCount: 2, AgentCount: 2234 },
-    //   { StepCount: 3, AgentCount: 3456 },
-    //   { StepCount: 4, AgentCount: 5678 },
-    //   { StepCount: 5, AgentCount: 7891 },
-    //   { StepCount: 6, AgentCount: 9012 },
-    //   { StepCount: 7, AgentCount: 10123 },
-    // ];
-
-    useEffect(() => {
-      webSocket.current = new WebSocket("ws://localhost:8080/ExperimentData");
-      webSocket.current.onopen = () => {
-        console.log("Connected to server");
-      }
-      webSocket.current.onmessage = (event) => {
-        const data = JSON.parse(event.data);
-        // console.log(data);
-        setSessionData((current) => [...current, data]);
-      }
-      webSocket.current.onclose = () => {
-        console.log("Disconnected from server");
-        webSocket.current.close();
-      }
-      return () => {
-        webSocket.current.close();
-      }
-    }, []);
-
-    // console.log(sessionData[sessionData.length - 1]);
-
-    useEffect(() => {
-      if (sessionData.length === 0) {
-        setPreviousResults([{
+  useEffect(() => {
+    if (sessionData.length === 0) {
+      setPreviousResults([
+        {
           CycleCount: 0,
           DooderCount: 0,
           EnergyCount: 0,
-          DirectionCounts: {}
-        }]);
-      } else {
-        setPreviousResults(sessionData[sessionData.length - 1]);
-      }
-    }, [sessionData]);
+          DirectionCounts: {},
+        },
+      ]);
+    } else {
+      setPreviousResults(sessionData[sessionData.length - 1]);
+    }
+  }, [sessionData]);
 
-  
   return (
     <Box>
-      <Toolbar socket={webSocket} setter={setSessionData}/>
-      <Dashboard data={previousResults}/>
+      <Toolbar socket={webSocket} setter={setSessionData} />
+      <Dashboard data={previousResults} />
       <ExperimentChart data={sessionData} />
-      <ChartApp />
+      <Grid>
+        <EnergyCycle /> 
+        <DooderCycle />
+      </Grid>
     </Box>
   );
 };
