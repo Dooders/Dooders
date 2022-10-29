1e44cca7f914faca914b27884ebcd7fc9446d4b8
Chris Mangum <csmangum@gmail.com>
2022-10-29 13:27:55 -0700

    Update models.py

--- db/models.py
+++ db/models.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.types import DateTime, Integer, String, Text
+from sqlalchemy.dialects import postgresql
 
 Base = declarative_base()
 
