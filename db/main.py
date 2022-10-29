5cfea4137141672f691546e43fd08cf2b65144ed
Chris Mangum <csmangum@gmail.com>
2022-10-28 19:22:43 -0700

    Update main.py

--- db/main.py
+++ db/main.py
@@ -26,10 +26,25 @@ class DB:
 
         return engine
     
-    def add_record(self, record_type):
-        # have class that when init it returns a desired result. like a vending machine. This class sacrifices itself. send a class and do something based on the type of class. basically encoding then decoding
-        record = RecordTypes(record_type)
+    def df_to_db(self, df, record_type):
+        # maybe have a decorator that creates and closes a session
+        df_dict = df.to_dict('rows')
         
+        self.session = self.create_session()
+        
+        for record in df_dict.values():
+            serialized_record = RecordTypes(record, record_type) 
+            self._add_record(serialized_record)
+            
+        self.session.commit()
+        self.session.close()
+    
+    def add_record(self, record):
+        # have class that when init it returns a desired result. like a vending machine. This class sacrifices itself. send a class and do something based on the type of class. basically encoding then decoding
+        
+        self.session.add(record)
+        
+    def _add_record(self, record):
         self.session.add(record)
     
     def reset(self):
