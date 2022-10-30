d48b13d0eb1f73c70af582b4f50b099a446e4720
Chris Mangum <csmangum@gmail.com>
2022-10-29 22:49:14 -0700

    Update base_policy.py

--- sdk/base/base_policy.py
+++ sdk/base/base_policy.py
@@ -1,1 +1,11 @@
+from abc import ABC, abstractmethod
 
+class BasePolicy(ABC):
+    """ 
+    """
+
+    @abstractmethod
+    def execute(self, dooder) -> Any:
+      
+      raise NotImplementedError
+       
