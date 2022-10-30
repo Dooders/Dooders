7704cdf475ce4fd8964dedebde8189d49a011ca9
Chris Mangum <csmangum@gmail.com>
2022-10-29 22:40:32 -0700

    Update policies.py

--- sdk/core/policies.py
+++ sdk/core/policies.py
@@ -1,6 +1,30 @@
 
-# has a wrapper to create policies
-# manages all policies and their execution
+
+
+
 
 class Policies:
-  pass
+
+    policies = {}
+    
+    def __init__(self):
+      # import all the policies from policy fooder
+      pass
+
+    @classmethod
+    def register(cls) -> Callable:
+        """ 
+        """
+
+        def inner_wrapper(wrapped_class: Callable) -> Callable:
+            cls.policies[wrapped_class.__name__] = wrapped_class
+
+            return wrapped_class
+
+        return inner_wrapper
+    
+    def __call__(self, policy, object):
+        feched_policy = self.policies[policy]
+        policy_results = feched_policy.execute(object)
+        
+        return policy_results
