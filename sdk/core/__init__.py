3eaf278ca72db54bd8cd7f30888770ae983cd446
Chris Mangum <csmangum@gmail.com>
2022-10-31 17:48:59 -0700

    implement policies

--- sdk/core/__init__.py
+++ sdk/core/__init__.py
@@ -1,6 +1,7 @@
 from sdk.core.collector import Collector as Collector
 from sdk.core.condition import Condition as Condition
 from sdk.core.strategy import Strategy as Strategy, compile_strategy as compile_strategy
+from sdk.core.policies import Policies as Policies
 
 # import sdk.collectors
 # import sdk.conditions
