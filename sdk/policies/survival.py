e58bf6d6a4f9e24c2c23a0f6b897b340432758b6
Chris Mangum <csmangum@gmail.com>
2022-11-08 22:19:50 -0700

    Update survival.py

--- sdk/policies/survival.py
+++ sdk/policies/survival.py
@@ -1,6 +1,3 @@
-# looking to consume energy. max or min a goal, like survival days
-# base goals: consume energy to maximize cycles, reproduce
-
 from random import choice
 from typing import Callable
 
@@ -50,8 +47,9 @@ class RuleBased(BasePolicy):
     
 @Policies.register()
 class NeuralNetwork(BasePolicy):
-    # policies can take control of the object and do whatever is needed.
-    # self contained functionality
+    """
+    
+    """
     
     @classmethod
     def execute(self, dooder) -> tuple:
@@ -59,13 +57,10 @@ class NeuralNetwork(BasePolicy):
         energy_list = [1 if isinstance(n, Energy) else 0 for n in neighbors]
         
         
-    # random, rule-based, NNs
     # maybe new dooders get a product of weights from parents.
     # genetic starting weights, learned weights. get product of that and those weights combine with another dooder during repro
     # genetic weights are a sequence of weights for other activities and policies. all can be contained in single array (with partitions)
 
-    # maybe this recomemds to always be comsuming. it can be base policy that can be overcome/overpowered
-    # weights that led to survive longer,
     
 # need to make a dataclass for the required output of the policy
 # maybe even a dataclass for the input
