68c68414794086a30acdc466d0f679dee6ae5017
Chris Mangum <csmangum@gmail.com>
2022-11-11 21:32:15 -0700

    Update neighborhood.py

--- sdk/modules/neighborhood.py
+++ sdk/modules/neighborhood.py
@@ -1,1 +1,11 @@
+# will contain list of location objects
+# slicing
+# mappimg to compass directions
+# turn list of locations to: list of location coordinates, list of energy/dooder objects, return if conditionals
 
+class Neighborhood(list):
+  
+  __mapping__ = {}
+  
+  
+  
