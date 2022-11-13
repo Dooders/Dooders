e0f1a8fb2a346ba9f8469a833c82688a02c901d4
Chris Mangum <csmangum@gmail.com>
2022-11-12 17:12:40 -0700

    adding location and neighborhood classes

---
+++ sdk/modules/location.py
@@ -0,0 +1,70 @@
+""" 
+Location class
+"""
+
+import random
+from sdk.models import Energy, Dooder
+
+
+class Location:
+
+    # List of all contents in the location
+    contents: str = {}
+
+    def __init__(self, x, y):
+        """ 
+        Initialize a location object with a unique id and coordinates
+
+        Args:
+            x: The x coordinate of the location
+            y: The y coordinate of the location
+
+        Returns:
+            A location object
+        """
+        self.x = x
+        self.y = y
+        self.coordinates = (x, y)
+        self.status = 'empty'
+
+    def add(self, object):
+        """ 
+        Add an object to the location
+
+        Args:
+            object: The object to add to the location
+        """
+        self.contents[object.unique_id] = object
+        self.status = 'occupied'
+
+    def remove(self, object):
+        """ 
+        Remove an object from the location
+
+        Args:
+            object: The object to remove from the location
+        """
+        self.contents.pop(object.unique_id, None)
+        if len(self.contents) == 0:
+            self.status = 'empty'
+
+    @property
+    def count(self):
+        """ 
+        Return the number of objects in the location
+        """
+        return len(self.contents)
+
+    @property
+    def random(self):
+        """ 
+        Return a random object in the location
+        """
+        return random.choice(list(self.contents.values()))
+
+    @property
+    def has(self, object_type):
+        """ 
+        Return a boolean indicating if the location contains a specific object type
+        """
+        return any([isinstance(x, object_type) for x in self.contents])
--- sdk/modules/neighborhood.py
+++ sdk/modules/neighborhood.py
@@ -1,11 +1,41 @@
-# will contain list of location objects
 # slicing
-# mappimg to compass directions
 # turn list of locations to: list of location coordinates, list of energy/dooder objects, return if conditionals
 
+import random
+
+from sdk.models import Energy, Dooder
+
+
 class Neighborhood(list):
-  
-  __mapping__ = {}
-  
-  
-  
+
+    __mapping__ = {0: 'N', 1: 'NE', 2: 'E', 3: 'SE',
+                   4: 'S', 5: 'SW', 6: 'W', 7: 'NW', 8: '-'}
+
+    def to_direction(self, location):
+        return self.__mapping__[self.index(location)]
+
+    def to_location(self, direction):
+        return self[self.__mapping__.index(direction)]
+
+    def contains(self, object_type):
+        return [1 if isinstance(location, object_type) else 0 for location in self]
+
+    @property
+    def energy(self):
+        return any([isinstance(x, Energy) for x in self])
+
+    @property
+    def dooder(self):
+        return any([isinstance(x, Dooder) for x in self])
+
+    @property
+    def locations(self):
+        return self
+
+    @property
+    def coordinates(self):
+        return [x.coordinates for x in self]
+
+    @property
+    def random(self):
+        return random.choice(self)
