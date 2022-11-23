69e83383a49f52d3bcc6c18b82b5213f4ac2757c
Chris Mangum <peril10@hotmail.com>
2022-11-23 13:20:58 -0700

    refactor, comment, clean

--- sdk/base/base_time.py
+++ sdk/base/base_time.py
@@ -1,36 +1,34 @@
 from abc import ABC, abstractmethod
-from typing import Type
-
-ClassObject = Type[ABC]
 
 
 class BaseTime(ABC):
     """ 
-
+    Base class for the Time model.
+    Having this class makes it simpler to experiment with different time logic
     """
 
     @abstractmethod
-    def add(self, object: ClassObject) -> None:
+    def add(self, object: object) -> None:
         """
         Add an object to the schedule.
 
         Args:
             object: Object to add to the schedule
         """
-        pass
+        raise NotImplementedError("Subclass must implement this method")
 
     @abstractmethod
-    def remove(self, object: ClassObject) -> None:
+    def remove(self, object: object) -> None:
         """
         Remove all instances of a given agent from the schedule.
 
         Args:
             agent: An agent object.
         """
-        pass
+        raise NotImplementedError("Subclass must implement this method")
 
     @abstractmethod
-    def _step(self, object_class: Type[ClassObject], shuffle_objects: bool = True) -> None:
+    def _step(self, object_class: object, shuffle_objects: bool = True) -> None:
         """ 
         Step through the objects of a given class
 
@@ -38,7 +36,7 @@ class BaseTime(ABC):
             object_class: Class of objects to step through
             shuffle_objects: Whether to shuffle the objects before stepping through them
         """
-        pass
+        raise NotImplementedError("Subclass must implement this method")
 
     @abstractmethod
     def step(self, shuffle_types: bool = True, shuffle_objects: bool = True) -> None:
@@ -49,4 +47,4 @@ class BaseTime(ABC):
             shuffle_types: Whether to shuffle the types of objects before stepping through them
             shuffle_objects: Whether to shuffle the objects before stepping through them
         """
-        pass
+        raise NotImplementedError("Subclass must implement this method")
