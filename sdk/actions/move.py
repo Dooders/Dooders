6c2230df2301cb5a9903dfda64ab79c8ac358e38
Chris Mangum <csmangum@gmail.com>
2022-12-15 22:49:47 -0700

    making actions

--- sdk/actions/move.py
+++ sdk/actions/move.py
@@ -1,17 +1,18 @@
 from sdk.core.actions import Actions
+from sdk.utils.get_direction import get_direction
 
 
 @Actions.register()
 def move(dooder):
-  policy = dooder.simulation.params.get('Policies').Movement
-  destination = dooder.simulation.policies(policy, dooder)
-  
-  if destination == dooder.position:
-            pass
-        else:
-            origin = dooder.position
-            dooder.direction = get_direction(origin, destination)
-            dooder.simulation.environment.move_object(dooder, destination)
-            dooder.log(
-                granularity=2, message=f"Moved {dooder.direction} from {origin} to {destination}", scope='Dooder')
-            dooder.position = destination
+    policy = dooder.simulation.params.get('Policies').Movement
+    destination = dooder.simulation.policies(policy, dooder)
+
+    if destination == dooder.position:
+        pass
+    else:
+        origin = dooder.position
+        dooder.direction = get_direction(origin, destination)
+        dooder.simulation.environment.move_object(dooder, destination)
+        dooder.log(
+            granularity=2, message=f"Moved {dooder.direction} from {origin} to {destination}", scope='Dooder')
+        dooder.position = destination
--- sdk/actions/reproduce.py
+++ sdk/actions/reproduce.py
@@ -1,4 +1,3 @@
-import copy
 from sdk.core.actions import Actions
 
 
