cd61a998d9bc3ae02c0c5c528efd29e86a19c61d
Chris Mangum <peril10@hotmail.com>
2022-12-15 21:36:32 -0700

    adding actions class

--- sdk/core/actions.py
+++ sdk/core/actions.py
@@ -1,8 +1,26 @@
-from sdk.actions import *
+from typing import Callable
+
+from sdk import actions
+
 
 class Actions:
-  # register actions in actions folder
-  # method to execute action
-  # new instance for each dooder
-  
-  pass
+
+    actions = {}
+
+    @classmethod
+    def register(cls) -> Callable:
+        """ 
+        """
+
+        def inner_wrapper(wrapped_class: Callable) -> Callable:
+            cls.actions[wrapped_class.__name__] = wrapped_class
+
+            return wrapped_class
+
+        return inner_wrapper
+
+    def __call__(self, object, action):
+        matched_action = self.actions[action]
+        action_results = matched_action.execute(object)
+
+        return action_results
