6ce17ea7e372297e0bb7702cd521c129e03b69e3
Chris Mangum <csmangum@gmail.com>
2022-12-15 22:17:27 -0700

    Update move.py

--- sdk/actions/move.py
+++ sdk/actions/move.py
@@ -2,5 +2,16 @@ from sdk.core.actions import Actions
 
 
 @Actions.register()
-def move(dooderA):
-  pass
+def move(dooder):
+  policy = dooder.simulation.params.get('Policies').Movement
+  destination = dooder.simulation.policies(policy, dooder)
+  
+  if destination == dooder.position:
+            pass
+        else:
+            origin = dooder.position
+            dooder.direction = get_direction(origin, destination)
+            dooder.simulation.environment.move_object(dooder, destination)
+            dooder.log(
+                granularity=2, message=f"Moved {dooder.direction} from {origin} to {destination}", scope='Dooder')
+            dooder.position = destination
