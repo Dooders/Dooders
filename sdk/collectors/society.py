288b488247f3d8a3e6e645f49271243c325027e0
Chris Mangum <csmangum@gmail.com>
2022-12-31 14:16:22 -0700

    Update society.py

--- sdk/collectors/society.py
+++ sdk/collectors/society.py
@@ -1,1 +1,7 @@
+from sdk.core.collector import Collector
 
+
+@Collector.register('ActiveDooderCount')
+def get_dooder_count(simulation) -> int:
+    """Return the number of dooders in the simulation."""
+    return simulation.society.active_dooder_count
