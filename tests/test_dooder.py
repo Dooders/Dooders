66e19bff7fd18b6880424f5905b60e926d8e2445
Chris Mangum <csmangum@gmail.com>
2023-01-04 21:15:10 -0700

    copied over to new repo

--- tests/test_dooder.py
+++ tests/test_dooder.py
@@ -28,35 +28,14 @@ def test_dooder_move(dooder):
     assert object.position == (2,2)
 
 
-def test_dooder_kill(dooder):
-    object, _ = dooder
-    other_dooder = mock_dooder()
-    object.kill(other_dooder)
-
-
 def test_dooder_die(dooder):
     dooder = mock_dooder()
     dooder.die()
 
 
-def test_dooder_choose_random_move(dooder):
-    object, _ = dooder
-    origin, destination = object.choose_random_move()
-    assert origin == (1,1)
-    assert destination in [(1,2), (2,1), (2,2), (1,1), (1,0), (0,1), (0,0), (2,0), (0,2), (2,2)]
-
-
 def test_dooder_step(dooder):
     object, _ = dooder
     object.step()
-    
-    
-def test_death_check(dooder):
-    dooder = mock_dooder()
-    assert dooder.death_check() == False
-    dooder.days_hungry = 3
-    dooder.energy_supply = 0
-    assert dooder.death_check() == True
     
     
 def test_str(dooder):
