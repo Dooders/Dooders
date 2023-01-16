0621baa312a2dbe368f95c99e8edf5efe346c0e4
Chris Mangum <csmangum@gmail.com>
2023-01-15 18:28:45 -0700

    improved environment model

--- sdk/collectors/environment.py
+++ sdk/collectors/environment.py
@@ -1,12 +1,26 @@
 from sdk.core.collector import Collector
 
 
-# @Collector.register('LocationContents')
-# def location_contents(simulation) -> dict:
-  
-#     location_contents = {}
-    
-#     for location in simulation.environment.locations.values():
-#         location_contents[location.coordinates] = location.contents
-    
-#     return location_contents
+@Collector.register('LocationContentsCount')
+def location_contents_count(simulation) -> dict:
+    """ 
+    Returns a dictionary of the number of contents in each location.
+
+    Sample Output
+    ------------
+    Coordinate: Contents count
+    {
+        (0, 0): 0,
+        (0, 1): 0,
+        (0, 2): 0,
+        (0, 3): 0
+    }
+    """
+
+    location_contents = {}
+
+    for row in simulation.environment.grid:
+        for location in row:
+            location_contents[location.coordinates] = len(location.contents)
+
+    return location_contents
