6d0af2f9cc8f787f3aca88012a3ae51b6277f58d
Chris Mangum <peril10@hotmail.com>
2023-03-12 20:14:59 -0700

    added class

---
+++ dooders/investigate.py
@@ -0,0 +1,30 @@
+import json
+import pandas as pd
+
+
+class Investigate:
+    
+    def __init__(self, experiment_name):
+        self.experiment_name = experiment_name
+        self.state = self.load_state("experiments/" + experiment_name, "state.json")
+        self.log = self.load_log("experiments/" + experiment_name + "/log.json")
+        self.data = self.state['information']
+
+    def load_log(self, file_path):
+        with open(file_path, 'r') as f:
+            lines = [eval(line.strip())[0] for line in f]
+            
+        df = pd.DataFrame(lines)
+        
+        return df
+    
+    def load_state(self, folder_path, file_name):
+        # Construct the full path to the JSON file
+        file_path = folder_path + "/" + file_name
+        
+        # Open the file and read its contents
+        with open(file_path, "r") as f:
+            json_data = json.load(f)
+        
+        # Return the contents as a dictionary
+        return json_data
