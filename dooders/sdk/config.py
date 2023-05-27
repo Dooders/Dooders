f39332e3547cd69f82d14263ea0ca7c32e2edc3c
Chris Mangum <csmangum@gmail.com>
2023-05-27 16:47:13 -0700

    added new config class

--- dooders/sdk/config.py
+++ dooders/sdk/config.py
@@ -1,10 +1,8 @@
+from dataclasses import dataclass
+from typing import Callable, Dict, List
+
 import yaml
-from typing import Callable, List
-
 from pydantic import BaseModel, Field
-
-#! Need better config, and a way to change settings on the fly
-#! will also need a settings view on the front-end
 
 
 class BaseCollector(BaseModel):
@@ -13,8 +11,8 @@ class BaseCollector(BaseModel):
     function: Callable = Field(..., description="Function to be called")
     component: str = Field(..., description="Component type to collect")
     
-with open("sdk/config.yml", "r") as f:
-      default_config = yaml.load(f, Loader=yaml.FullLoader)
+# with open("sdk/config.yml", "r") as f:
+#       default_config = yaml.load(f, Loader=yaml.FullLoader)
 
 
 
@@ -55,8 +53,6 @@ class PolicyParameters(BaseModel):
     Reproduction: str = Field("AverageWeights", description="Reproduction policy")
     
     
-    
-    
 class ExperimentParameters(BaseModel):
     """ Parameters for the Experiment """
     Simulation = SimulationParameters()
@@ -77,3 +73,67 @@ class ExperimentParameters(BaseModel):
             object: Value of the key
         """
         return self.__dict__[key]
+    
+
+@dataclass
+class ValueGenerator:
+    distribution_type: str
+    min_value: int
+    max_value: int
+    
+class Config:
+    """ 
+    Class to hold the settings for the simulation
+    
+    Attributes
+    ----------
+    settings : dict
+        Dictionary of settings
+        
+    Methods
+    -------
+    update(new_settings: dict) -> None
+        Update the settings with new settings
+    get(setting_name: str) -> object
+        Get a setting from the settings
+    """
+    def __init__(self) -> None:
+        self.settings: dict = {
+            'MaxCycles': 100,
+            'SeedCount': 1,
+            'EnergyPerCycle': ValueGenerator('uniform', 5, 10),
+            'MaxTotalEnergy': 10,
+            'GridHeight': 5,
+            'GridWidth': 5,
+            'EnergyLifespan': ValueGenerator('uniform', 2, 5),
+        }
+
+    def update(self, new_settings: dict) -> None:
+        """ 
+        Update the settings with new settings
+        
+        Parameters
+        ----------
+        new_settings : dict
+            New settings to update the current settings with
+        """
+        for key, value in new_settings.items():
+            if key in self.settings:
+                self.settings[key] = value
+            else:
+                raise KeyError(f"{key} does not exist in the settings")
+
+    def get(self, setting_name: str) -> object:
+        """ 
+        Get a setting from the settings
+        
+        Parameters
+        ----------
+        setting_name : str
+            Name of the setting to get
+        """
+        try:
+            return self.settings[setting_name]
+        except KeyError:
+            raise KeyError(f"{setting_name} does not exist in the settings")
+
