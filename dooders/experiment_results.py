623ea20523af907d93e35ed499b22f5ec25e185e
Chris Mangum <peril10@hotmail.com>
2023-06-01 20:17:19 -0700

    added data functions and new viz

---
+++ dooders/experiment_results.py
@@ -0,0 +1,41 @@
+def total_accuracy(inference_record):
+    results = [a['accurate']
+               for a in inference_record.values() if a['accurate'] is not None]
+    true_count = sum(results)
+    total_count = len(results)
+    percentage = (true_count / total_count) * 100 if total_count > 0 else 0.0
+    return percentage, total_count
+
+
+def running_accuracy(inference_record):
+    count_true = 0
+    total = 0
+    accuracies = []
+
+    for value in inference_record.values():
+        total += 1
+        if value['accurate']:
+            count_true += 1
+        accuracies.append(count_true / total)
+
+    return accuracies
+
+
+def n_running_accuracy(inference_record, n=200):
+    count_true = 0
+    total = 0
+    accuracies = []
+    record_items = list(inference_record.items())
+
+    for i, (_, value) in enumerate(record_items):
+        total += 1
+        if value['accurate']:
+            count_true += 1
+        if i >= n:
+            oldest_item = record_items[i - n]
+            if oldest_item[1]['accurate']:
+                count_true -= 1
+            total -= 1
+        accuracies.append(count_true / total)
+
+    return accuracies
---
+++ dooders/viz/gene_embedding.py
@@ -0,0 +1,44 @@
+import plotly.express as px
+
+
+def gene_embedding(gene_embeddings_df, color_by='cycle', show_cbar=False) -> px.scatter:
+    """ 
+    Create a scatter plot of the gene embeddings.
+    
+    Parameters
+    ----------
+    gene_embeddings_df : pd.DataFrame
+        A DataFrame containing the gene embeddings.
+    color_by : str
+        The column name to color the points by.
+    show_cbar : bool
+        Whether to show the colorbar.
+
+    Returns
+    -------
+    fig : px.scatter
+        The scatter plot.
+    """
+    # Create the scatter plot
+    fig = px.scatter(gene_embeddings_df,
+                     x="X",
+                     y="Y",
+                     color=color_by,
+                     color_continuous_scale="Viridis")
+
+    # Remove axis titles
+    fig.update_xaxes(title=None)
+    fig.update_yaxes(title=None)
+
+    # Remove tick labels
+    fig.update_xaxes(showticklabels=False)
+    fig.update_yaxes(showticklabels=False)
+
+    # Remove the color legend
+    fig.update_layout(coloraxis_showscale=False)
+
+    if show_cbar:
+        fig.update_layout(coloraxis_colorbar=dict(
+            title=color_by))
+
+    return fig
