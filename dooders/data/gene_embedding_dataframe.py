26a1ec83a2b97625841b359ea04602fe2a5b8f1f
Chris Mangum <csmangum@gmail.com>
2023-06-04 21:17:33 -0700

    added data functions

---
+++ dooders/data/gene_embedding_dataframe.py
@@ -0,0 +1,37 @@
+import pandas as pd
+
+
+def get_gene_embedding_df(experiment_results: dict) -> pd.DataFrame:
+    """ 
+    Returns a dataframe of all dooders in the experiment results.
+
+    Parameters
+    ----------
+    experiment_results : dict
+        The experiment results dictionary.
+
+    Returns
+    -------
+    gene_embedding_df : pd.DataFrame
+        A dataframe of all dooders in the experiment results.
+    """
+
+    gene_embedding_records = []
+
+    for data in experiment_results.values():
+        arena_data = data['state']['arena']
+        for dooder in arena_data.values():
+            for cycle, record in dooder['encoded_weights'].items():
+                temp = {
+                    'id': dooder['id'],
+                    'cycle': cycle,
+                    'X': record[0],
+                    'Y': record[1],
+                    'Z': record[2]
+                }
+                gene_embedding_records.append(temp)
+
+    columns = list(gene_embedding_records[0].keys())
+    gene_embedding_df = pd.DataFrame(gene_embedding_records, columns=columns)
+    
+    return gene_embedding_df
---
+++ dooders/data/inference_record_dataframe.py
@@ -0,0 +1,35 @@
+import pandas as pd 
+
+
+def get_inference_record_df(experiment_results: dict) -> pd.DataFrame:
+    """ 
+    Returns a dataframe of all dooders in the experiment results.
+    
+    Parameters
+    ----------
+    experiment_results : dict
+        The experiment results dictionary.
+
+    Returns
+    -------
+    inference_record_df : pd.DataFrame
+        A dataframe of all dooders in the experiment results.
+    """
+    inference_records = []
+
+    for simulation_key in experiment_results:
+        arena_dict = experiment_results.get(simulation_key)['state']['arena']
+        
+        for key in arena_dict:
+            inference_record = arena_dict.get(key)['inference_record']
+
+            record_list = []
+            for record in inference_record:
+                single_record = inference_record.get(record)
+                single_record['cycle'] = int(record)
+                single_record['dooder'] = key
+                inference_records.append(single_record)
+                
+    inference_df = pd.DataFrame(inference_records)
+    
+    return inference_df
