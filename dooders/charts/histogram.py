b8c3eed1da9de6feb1e3e6bd955351ea843dc0e2
Chris Mangum <csmangum@gmail.com>
2023-06-22 18:23:43 -0700

    added function

--- dooders/charts/histogram.py
+++ dooders/charts/histogram.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import plotly.graph_objects as go
+import plotly.express as px
+
 
 
 def death_age_by_cycle(dooder_df: pd.DataFrame) -> go.Figure:
@@ -43,3 +45,31 @@ def death_age_by_cycle(dooder_df: pd.DataFrame) -> go.Figure:
     figure = go.Figure(data=[trace], layout=layout)
 
     return figure
+
+def starting_success_probability(dooder_df: pd.DataFrame) -> go.Figure:
+    """ 
+    Creates a histogram of the success probability of the dooders.
+    
+    Parameters
+    ----------
+    dooder_df : pd.DataFrame
+        The DataFrame containing the information about the dooders.
+
+    Returns
+    -------
+    figure : go.Figure
+        The histogram of the success probability of the dooders.
+    """
+
+    fig = px.histogram(dooder_df, x="starting_success_probability", color='experiment_result')
+
+    # Update title
+    fig.update_layout(title="Histogram of Starting Success Probability")
+
+    # Update x-axis label
+    fig.update_xaxes(title="Starting Success Probability")
+
+    # Update y-axis label
+    fig.update_yaxes(title="Dooder Count")
+    
+    return fig
