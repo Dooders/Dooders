4f1063b31b71d298675f5da7bd1355b3dd6a0c10
Chris Mangum <peril10@hotmail.com>
2023-07-14 22:03:24 -0700

    commented out

--- dooders/selection.py
+++ dooders/selection.py
@@ -8,51 +8,52 @@ from dooders.experiment import Experiment
 from dooders.sdk.modules.recombination import recombine
 
 
-def weight_file_list(path: str) -> list:
-    """ 
-    Returns a list of all the weight files in a directory
+#! Commented out for now, but may be useful later
+# def weight_file_list(path: str) -> list:
+#     """
+#     Returns a list of all the weight files in a directory
 
-    Parameters
-    ----------
-    path : (str)
-        The path to the directory containing the weight files
+#     Parameters
+#     ----------
+#     path : (str)
+#         The path to the directory containing the weight files
 
-    Returns
-    -------
-    files : (list)
-        A list of all the weight files in the directory
-    """
-    files = []
+#     Returns
+#     -------
+#     files : (list)
+#         A list of all the weight files in the directory
+#     """
+#     files = []
 
-    for file in glob.glob(path + "*"):
-        if "Consume" in file:
-            files.append(file)
+#     for file in glob.glob(path + "*"):
+#         if "Consume" in file:
+#             files.append(file)
 
-    return files
+#     return files
 
 
-def random_dooder_weights(files: list) -> tuple:
-    """ 
-    Returns a random dooder's weights from a list of weight files
+# def random_dooder_weights(files: list) -> tuple:
+#     """
+#     Returns a random dooder's weights from a list of weight files
 
-    Parameters
-    ----------
-    files : (list)
-        A list of all the weight files in the directory
+#     Parameters
+#     ----------
+#     files : (list)
+#         A list of all the weight files in the directory
 
-    Returns
-    -------
-    dooder_id : (str)
-        The id of the dooder whose weights were selected
-    weights : (np.ndarray)
-        The weights of the dooder whose weights were selected
-    """
+#     Returns
+#     -------
+#     dooder_id : (str)
+#         The id of the dooder whose weights were selected
+#     weights : (np.ndarray)
+#         The weights of the dooder whose weights were selected
+#     """
 
-    random_file = random.choice(files)
-    dooder_id = random_file.split('\\')[1].split('_')[0]
-    weights = np.load(random_file, allow_pickle=True)
+#     random_file = random.choice(files)
+#     dooder_id = random_file.split('\\')[1].split('_')[0]
+#     weights = np.load(random_file, allow_pickle=True)
 
-    return dooder_id, weights
+#     return dooder_id, weights
 
 
 # def random_parents(path: str) -> tuple:
