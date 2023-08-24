9e4ba0715763be6977b4a929cf68a2d4de9e975d
Chris Mangum <peril10@hotmail.com>
2023-08-24 15:13:34 -0700

    improved weight property

--- dooders/sdk/models/arena.py
+++ dooders/sdk/models/arena.py
@@ -306,7 +306,7 @@ class Arena:
         """
         Returns the weights of the Arena for all active dooders
         """
-        return [v.weights['energy_detection'] for v in self.active_dooders.values()]
+        return [v.weights for v in self.active_dooders.values()]
 
     @property
     def current_cycle(self) -> int:
