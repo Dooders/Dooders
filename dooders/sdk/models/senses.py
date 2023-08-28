2e5a33b9b1bb3984e4c49c9094d4b7862969f22e
Chris Mangum <csmangum@gmail.com>
2023-08-28 16:35:32 -0700

    cleaned up

--- dooders/sdk/models/senses.py
+++ dooders/sdk/models/senses.py
@@ -19,11 +19,11 @@ class Senses:
         Gather the sensory array from the dooder's perception
     """
     SENSE_TYPES = {
-    'energy_detection': 'Energy',
-    # 'dooder_detection': 'Dooder',
-    # hazard_detection: 'Hazard',
+        'energy_detection': 'Energy',
+        # 'dooder_detection': 'Dooder',
+        # hazard_detection: 'Hazard',
     }
-    
+
     @classmethod
     def gather(cls, dooder) -> list:
         """ 
@@ -43,10 +43,12 @@ class Senses:
         for model_name in cls.SENSE_TYPES:
             object_name = cls.SENSE_TYPES[model_name]
             perception_array = dooder.perception.array(object_name)
-            result = dooder.think(model_name, perception_array)
-            sensory_array.append(result)
-            
-        combined_array = np.concatenate([arr.squeeze() for arr in sensory_array])
-            
-        return np.array([combined_array], dtype='float32')
 
+            result = dooder.think(model_name, perception_array)
+
+            sensory_array.append(result)
+
+        combined_array = np.concatenate(
+            [arr.squeeze() for arr in sensory_array])
+
+        return np.array([combined_array], dtype='float32')
