d05c0650655a14662e940e94c75a44cf113c9af1
Chris Mangum <csmangum@gmail.com>
2023-09-02 14:29:03 -0700

    Update circuit.py

--- dooders/sdk/core/circuit.py
+++ dooders/sdk/core/circuit.py
@@ -1,1 +1,10 @@
 
+# neural network circuits of internal models
+# i.e. the movement decision circuit that will use the senses internal models to determine where to move
+# as seen in primciples of neural design
+
+
+
+class MovementDecision:
+  def __init__(self):
+    pass
