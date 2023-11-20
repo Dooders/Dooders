5ca558be782843e178dc99590b9f03b64e61e262
Chris Mangum <csmangum@gmail.com>
2023-11-19 16:40:11 -0800

    Add default settings for Dooders

---
+++ dooders/defaults.py
@@ -0,0 +1,11 @@
+
+#! Have all the default settings here, use functions and dict to build out 
+#! further levels of settings
+
+max_cycle_count =  100
+dooder_count = 1
+energy_per_cycle = 10
+grid_height = 0
+grid_width = 10
+perception_input_size = 5
+perception_output_size = 5
