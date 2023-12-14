55068d0c961734ffdc293b8915c471f227dcb329
Chris Mangum <csmangum@gmail.com>
2023-12-14 13:21:44 -0700

    Add target attribute to Blinky class and update target based on current mode

--- dooders/game/blinky.py
+++ dooders/game/blinky.py
@@ -23,11 +23,14 @@ class Blinky(Entity):
         self.name = BLINKY
         self.color = RED
         self.alive = True
+        self.points = 200
         self.sprites = GhostSprites(self)
         self.spawn = Coordinate(13, 14)
         self.position = self.spawn
+        self.previous_position = self.position
         self.mode = ModeController(self)
         self.path = []
+        self.target = None
         self.waypoints = [
             (6, 17),
             (6, 4),
@@ -40,6 +43,17 @@ class Blinky(Entity):
             (1, 8),
         ]
 
+    def update_target(self, game) -> None:
+        if self.mode.current == SPAWN:
+            self.target = self.spawn
+
+        elif self.mode.current == CHASE:
+            self.target = game.pacman.position
+
+        elif self.mode.current == SCATTER:
+            if self.path == [] and self.waypoints != []:
+                self.target = self.waypoints.pop(0)
+
     def update(self, game) -> None:
         """
         Updates the ghost's position and direction based on the current mode.
@@ -50,10 +64,12 @@ class Blinky(Entity):
             The game controller object that the ghost is in.
         """
         dt = game.dt
-
+        current_position = self.position.copy()
         self.sprites.update(dt)
+        self.update_target(game)
         self.mode.update(dt)
         self.next_move(game)
+        self.previous_position = current_position
 
     def get_path(self, game) -> None:
         self.path = game.graph.path_finding(self.position, self.target)
@@ -70,27 +86,30 @@ class Blinky(Entity):
             self.position = next_position
 
     def next_move(self, game) -> None:
-        if self.path == [] and self.waypoints != []:
-            self.target = self.waypoints.pop(0)
-            self.get_path(game)
+        self.get_path(game)
 
-        if self.waypoints == []:
-            self.target = game.pacman.position
-            self.get_path(game)
+        if self.mode.current == FREIGHT:
+            if len(self.path) == 1:
+                self.target = game.pacman.position
 
-        if self.path != []:
-            next_position = self.path.pop(0)
-
-            if type(next_position) == tuple:
-                next_position = Coordinate(next_position[0], next_position[1])
-            self.direction = self.position.relative_direction(next_position)
-            self.position = next_position
+        self.move()
 
     def start_freight(self) -> None:
         """
         Starts the ghost's freight mode.
         """
         self.mode.set_freight_mode()
+        self.target = self.spawn
+
+    def start_spawn(self) -> None:
+        """
+        Sets the ghost's mode to spawn mode.
+        """
+        self.mode.set_spawn_mode()
+        self.target = self.spawn
+
+    def normal_mode(self) -> None:
+        pass
 
     def reset(self) -> None:
         """
