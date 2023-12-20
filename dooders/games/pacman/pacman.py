901140e33368a74663cf2f12d926608c2276b77d
Chris Mangum <csmangum@gmail.com>
2023-12-19 17:36:35 -0700

    Refactor PacMan class to use type hints and improve code readability

--- dooders/games/pacman/pacman.py
+++ dooders/games/pacman/pacman.py
@@ -1,6 +1,4 @@
-from typing import Union
-
-from pygame.locals import *
+from typing import TYPE_CHECKING, List, Union
 
 from dooders.games.npc import NPC
 from dooders.games.pacman.settings import Colors, Directions, SpawnPositions
@@ -9,20 +7,24 @@ from dooders.games.pacman.states import PacManState
 from dooders.games.pacman.targets import PacManTarget
 from dooders.sdk.base.coordinate import Coordinate
 
+if TYPE_CHECKING:
+    from dooders.games.pacman.game import Game
+    from dooders.games.pacman.pellets import Pellet
+
 
 class PacMan(NPC):
     """
-    PacMan is the main character of the game. He is controlled by the player
-    and must eat all the pellets in the maze while avoiding the ghosts.
+    PacMan is the main character of the game. He is autonomously controlled by
+    an AI and must eat all the pellets in the maze while avoiding the ghosts.
 
     Attributes
     ----------
     color : tuple
         The color of the entity
-    direction : int
-        The direction the entity is moving
     alive : bool
-        Whether or not the entity is alive
+        Whether the entity is alive or not
+    direction : Coordinate
+        The direction the entity is moving in
     sprites : PacManSprites
         The sprites for the entity
     spawn : Coordinate
@@ -33,33 +35,33 @@ class PacMan(NPC):
         The state of the entity
     previous_position : Coordinate
         The previous position of the entity
-    path : list
-        The path the entity is following
+    path : List[Coordinate]
+        The path the entity to reach its target
     target : PacManTarget
-        The target of the entity
+        The target the entity is currently following
 
     Methods
     -------
-    update(dt)
+    update(game: Game)
         Updates the entity
-    eat_pellets(pellet_List)
+    eat_pellets(pellet_List: List["Pellet"])
         Checks if the entity has eaten a pellet
     """
 
     def __init__(self) -> None:
-        NPC.__init__(self)
+        super().__init__()
         self.color = Colors.YELLOW.value
-        self.alive = True
+        self.alive: bool = True
         self.direction = Directions.STOP
         self.sprites = PacManSprites(self)
         self.spawn = Coordinate(SpawnPositions.PACMAN)
         self.position = self.spawn
         self.state = PacManState(self)
         self.previous_position = self.position
-        self.path = []
+        self.path: List[Coordinate] = []
         self.target = PacManTarget()
 
-    def update(self, game) -> None:
+    def update(self, game: "Game") -> None:
         """
         Updates the Pac-Man's state based on the time delta (dt).
 
@@ -69,11 +71,11 @@ class PacMan(NPC):
 
         Parameters
         ----------
-        dt : float
-            The time delta
+        game : Game
+            The game object
         """
-        dt = game.dt
-        self.sprites.update(dt)
+        time_delta = game.dt
+        self.sprites.update(time_delta)
 
         if self.alive:
             current_position = self.position.copy()
@@ -83,7 +85,7 @@ class PacMan(NPC):
             self.move()
             self.previous_position = current_position
 
-    def eat_pellets(self, pellet_List: list) -> Union[None, object]:
+    def eat_pellets(self, pellet_List: List["Pellet"]) -> Union[None, object]:
         """
         Checks for collisions between Pac-Man and any pellet in the provided list.
 
@@ -91,12 +93,12 @@ class PacMan(NPC):
 
         Parameters
         ----------
-        pellet_List : list
+        pellet_List : List["Pellet"]
             A list of pellets to check for collisions with
 
         Returns
         -------
-        object
+        Optional[Pellet]
             The pellet that was "eaten" if a collision is detected, None otherwise
         """
         for pellet in pellet_List:
