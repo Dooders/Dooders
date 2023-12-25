bd76eb2d523974e2fbfd54675587f0c43fd638e1
Chris Mangum <peril10@hotmail.com>
2023-12-24 18:51:20 -0700

    Add Simulation class with basic methods and properties

--- dooders/sdk/base/simulation.py
+++ dooders/sdk/base/simulation.py
@@ -1,1 +1,44 @@
-# Base class for creating simulations
+from abc import ABC, abstractmethod
+from dooders.sdk.utils import seed
+
+
+class Simulation(ABC):
+    def __init__(self, settings):
+        self.id = seed.uuid()
+        self.settings = settings
+        self.running = False
+        self.cycle_number: int = 0
+        self.results = {}
+
+    @abstractmethod
+    def setup(self) -> None:
+        pass
+
+    @abstractmethod
+    def update(self) -> None:
+        pass
+
+    @abstractmethod
+    def run_simulation(self, step_count: int) -> None:
+        pass
+
+    def cycle(self) -> None:
+        self.cycle += 1
+        self.cycle_number += 1
+        self.update()
+
+    def reset(self) -> None:
+        self.cycle_number = 0
+        self.results = {}
+
+    def stop(self) -> None:
+        self.running = False
+
+    @property
+    def state(self) -> dict:
+        #! represents the object data
+        pass
+
+    @property
+    def generate_id(self) -> str:
+        return seed.uuid()
